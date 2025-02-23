42549db1ca17fab74c676504e2120e6be9fa041d
jpcchaves <jpcchaves@outlook.com>
2025-02-23 18:57:19 -0300

    initial commit

---
+++ notification-service/src/test/java/com/financialmonitoring/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialmonitoring.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
