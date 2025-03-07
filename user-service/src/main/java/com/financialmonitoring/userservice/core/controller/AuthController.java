222ffaad13e762fea59f08929ae5734d5f0bbbba
jpcchaves <jpcchaves@outlook.com>
2025-03-07 16:48:44 -0300

    add login endpoint

--- user-service/src/main/java/com/financialmonitoring/userservice/core/controller/AuthController.java
+++ user-service/src/main/java/com/financialmonitoring/userservice/core/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.financialmonitoring.userservice.core.controller;
 
+import com.financialmonitoring.userservice.core.dto.LoginRequestDTO;
+import com.financialmonitoring.userservice.core.dto.LoginResponseDTO;
 import com.financialmonitoring.userservice.core.service.UserService;
+
+import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +23,11 @@ public class AuthController {
 
     public AuthController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO requestDTO) {
+        return ResponseEntity.ok(userService.login(requestDTO));
     }
 
     @GetMapping("/verify-token")
