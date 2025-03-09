1cf323500911b1ca614af9bc2889905c6f11a2cd
jpcchaves <jpcchaves@outlook.com>
2025-03-09 17:19:15 -0300

    move it to user service

--- user-service/src/main/java/com/financialmonitoring/userservice/core/utils/JwtUtils.java
+++ user-service/src/main/java/com/financialmonitoring/userservice/core/utils/JwtUtils.java
@@ -1,19 +1,22 @@
-package com.financialmonitoring.commonlib.utils;
+package com.financialmonitoring.userservice.core.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+@Component
 public class JwtUtils {
     private static final String BEARER_PREFIX = "Bearer ";
 
-    public static String getTokenFromRequest(HttpServletRequest request) {
+    public String getTokenFromRequest(HttpServletRequest request) {
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         return extractTokenFromHeader(authHeader);
     }
 
-    public static String extractTokenFromHeader(String authHeader) {
+    public String extractTokenFromHeader(String authHeader) {
         if (hasBearerToken(authHeader)) {
             return authHeader.replace(BEARER_PREFIX, "");
         }
@@ -21,7 +24,7 @@ public class JwtUtils {
         return null;
     }
 
-    private static boolean hasBearerToken(String authHeader) {
+    private boolean hasBearerToken(String authHeader) {
         return StringUtils.hasText(authHeader) && authHeader.startsWith(BEARER_PREFIX);
     }
 }
