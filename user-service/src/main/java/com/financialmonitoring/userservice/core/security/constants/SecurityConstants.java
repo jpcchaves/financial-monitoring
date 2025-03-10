64e9a2f473ad4d4b5f2252fc3b42688255317717
jpcchaves <jpcchaves@outlook.com>
2025-03-10 16:23:00 -0300

    add verify token as public route

--- user-service/src/main/java/com/financialmonitoring/userservice/core/security/constants/SecurityConstants.java
+++ user-service/src/main/java/com/financialmonitoring/userservice/core/security/constants/SecurityConstants.java
@@ -3,7 +3,9 @@ package com.financialmonitoring.userservice.core.security.constants;
 public class SecurityConstants {
 
     public static final String[] PUBLIC_ROUTES =
-            new String[] {"/api/v1/auth/register", "/api/v1/auth/login"};
+            new String[] {
+                "/api/v1/auth/register", "/api/v1/auth/login", "/api/v1/auth/verify-token"
+            };
 
     public static final int BCRYPT_SALT = 10;
 }
