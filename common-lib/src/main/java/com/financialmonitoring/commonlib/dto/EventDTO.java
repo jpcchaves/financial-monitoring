17f52b94566cfd200972054e341b39138b78bc30
jpcchaves <jpcchaves@outlook.com>
2025-03-24 22:25:55 -0300

    add event dto class

---
+++ common-lib/src/main/java/com/financialmonitoring/commonlib/dto/EventDTO.java
@@ -0,0 +1,125 @@
+package com.financialmonitoring.commonlib.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class EventDTO<T> implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7389224274356745896L;
+    
+    private String id;
+    private String transactionId;
+    private String source;
+    private T payload;
+    private List<HistoryDTO> eventHistory;
+    private LocalDateTime createdAt;
+
+    public EventDTO(Builder<T> builder) {
+        this.id = builder.id;
+        this.transactionId = builder.transactionId;
+        this.source = builder.source;
+        this.payload = builder.payload;
+        this.eventHistory = builder.eventHistory;
+        this.createdAt = builder.createdAt;
+    }
+
+    public static <T> Builder<T> builder() {
+        return new Builder<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public T getPayload() {
+        return payload;
+    }
+
+    public void setPayload(T payload) {
+        this.payload = payload;
+    }
+
+    public List<HistoryDTO> getEventHistory() {
+        return eventHistory;
+    }
+
+    public void setEventHistory(List<HistoryDTO> eventHistory) {
+        this.eventHistory = eventHistory;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public static class Builder<T> {
+
+        private String id;
+        private String transactionId;
+        private String source;
+        private T payload;
+        private List<HistoryDTO> eventHistory;
+        private LocalDateTime createdAt;
+
+        public Builder<T> id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder<T> transactionId(String transactionId) {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder<T> source(String source) {
+            this.source = source;
+            return this;
+        }
+
+        public Builder<T> payload(T payload) {
+            this.payload = payload;
+            return this;
+        }
+
+        public Builder<T> eventHistory(List<HistoryDTO> eventHistory) {
+            this.eventHistory = eventHistory;
+            return this;
+        }
+
+        public Builder<T> createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public EventDTO<T> build() {
+            return new EventDTO<>(this);
+        }
+
+    }
+}
