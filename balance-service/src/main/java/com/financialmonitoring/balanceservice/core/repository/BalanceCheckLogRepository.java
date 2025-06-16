5699368e71f20799b86c1f90d392ddb16538a1c8
jpcchaves <jpcchaves@outlook.com>
2025-06-16 09:31:13 -0300

    feat: add findByEventIdAndTransactionId method

--- balance-service/src/main/java/com/financialmonitoring/balanceservice/core/repository/BalanceCheckLogRepository.java
+++ balance-service/src/main/java/com/financialmonitoring/balanceservice/core/repository/BalanceCheckLogRepository.java
@@ -1,11 +1,17 @@
 package com.financialmonitoring.balanceservice.core.repository;
 
 import com.financialmonitoring.balanceservice.core.model.BalanceCheckLog;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BalanceCheckLogRepository extends JpaRepository<BalanceCheckLog, UUID> {
-    boolean existsByEventIdAndTransactionId(String eventId, String transactionId);
+
+    boolean existsByEventIdAndTransactionId(String eventId,
+            String transactionId);
+
+    Optional<BalanceCheckLog> findByEventIdAndTransactionId(String eventId,
+            String transactionId);
 }
