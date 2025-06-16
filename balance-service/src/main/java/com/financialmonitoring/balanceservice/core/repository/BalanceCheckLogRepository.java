87aa00901956c309a391f42aa22f5d64536e3678
jpcchaves <jpcchaves@outlook.com>
2025-06-15 22:34:51 -0300

    feat: add findBySenderId and exists by event id and transactionid

--- balance-service/src/main/java/com/financialmonitoring/balanceservice/core/repository/BalanceCheckLogRepository.java
+++ balance-service/src/main/java/com/financialmonitoring/balanceservice/core/repository/BalanceCheckLogRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BalanceCheckLogRepository extends JpaRepository<BalanceCheckLog, UUID> {
-
+    boolean existsByEventIdAndTransactionId(String eventId, String transactionId);
 }
