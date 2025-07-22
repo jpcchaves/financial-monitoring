4a3e18c241d3c27efa9e99797e9cd09e5c7c1188
jpcchaves <jpcchaves@outlook.com>
2025-07-21 21:14:28 -0300

    refactor: remove unnecessary repository annotation in out port

--- user-service/src/main/java/com/financialmonitoring/userservice/adapter/out/repository/SpringDataRoleRepository.java
+++ user-service/src/main/java/com/financialmonitoring/userservice/adapter/out/repository/SpringDataRoleRepository.java
@@ -3,15 +3,11 @@ package com.financialmonitoring.userservice.adapter.out.repository;
 import com.financialmonitoring.userservice.adapter.out.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface SpringDataRoleRepository extends JpaRepository<Role, Long> {
 
-    @Query(
-            value = "SELECT * FROM tb_roles r WHERE UPPER(r.name) = UPPER(:name)",
-            nativeQuery = true)
+    @Query(value = "SELECT * FROM tb_roles r WHERE UPPER(r.name) = UPPER(:name)", nativeQuery = true)
     Optional<Role> findByName(String name);
 }
--- user-service/src/main/java/com/financialmonitoring/userservice/adapter/out/repository/SpringDataUserRepository.java
+++ user-service/src/main/java/com/financialmonitoring/userservice/adapter/out/repository/SpringDataUserRepository.java
@@ -3,18 +3,14 @@ package com.financialmonitoring.userservice.adapter.out.repository;
 import com.financialmonitoring.userservice.adapter.out.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface SpringDataUserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "SELECT * FROM tb_users u WHERE u.email = :email", nativeQuery = true)
     Optional<User> findByEmail(String email);
 
-    @Query(
-            value = "SELECT EXISTS(SELECT 1 FROM tb_users u WHERE u.email = :email)",
-            nativeQuery = true)
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM tb_users u WHERE u.email = :email)", nativeQuery = true)
     Boolean existsByEmail(String email);
 }
